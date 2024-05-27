3a80bcfed2d7f74253e84845776f80158194d806
Edemowns <2000larryk@gmail.com>
2024-05-27 01:10:58 -1200

    my first commit

---
+++ DCIT202-expo-assignment/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>
+        My name is <Text style={styles.boldtext}>Nyanyo Edem Larry Kwaku</Text>
+        <StatusBar style="auto" />
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
